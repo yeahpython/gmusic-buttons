c98e3481b98bd0fb794c27ad11e027f2924587cb
Tommy Li <123tom.li@gmail.com>
2017-11-25 17:44:03 -0800

    Lots of finicky stuff with tracking state

--- extension/background.js
+++ extension/background.js
@@ -14,18 +14,112 @@ function try_to_play(id, callback) {
   });
 }
 var waiting_for_port_to_open = false;
-var persistent_port;
+// Never remove from these lists.
+var persistent_ports = [];
+var persistent_ports_status = [];
+var persistent_ports_last_change = [];
 var port_queue = [];
 function requestPlay() {
-  var message = {"play":"true"};
-  try {
-    persistent_port.postMessage(message);
-  } catch (err) {
-    // If we never connected or it disconnected
-    console.log(err);
-    persistent_post = undefined;
+  // State is paused -> play button is visible
+  var message = {"play": (badge_state !== "playing")};
+  console.log(message);
+  var found_port = false;
+
+  var decorated_change_timestamps = [];
+  for (var i = 0; i < persistent_ports_last_change.length; i++) {
+    decorated_change_timestamps.push({index:i, visible:persistent_ports_status[i].has_focus, time:persistent_ports_last_change[i]});
+    
+  }
+  if (message.play) {
+    // If one is visible, choose it first, otherwise use time.
+    var compfn = function(a,b) {
+      var first_diff = b.visible - a.visible;
+      if (first_diff) {
+        return first_diff;
+      }
+      // b goes first if it is bigger
+      return b.time - a.time;
+    }
+  } else {
+    // I swapped the arguments. Basically we are placing the most "prominent"
+    // things on the stack.
+    var compfn = function(b,a) {
+      var first_diff = b.visible - a.visible;
+      if (first_diff) {
+        return first_diff;
+      }
+      return b.time - a.time;
+    }
+  }
+  decorated_change_timestamps.sort(compfn);
+  // Iterate through ports by most recently modified
+  for (var j = 0; j < decorated_change_timestamps.length; j++) {
+    try {
+      var index = decorated_change_timestamps[j].index;
+
+      persistent_ports[index].postMessage(message);
+      found_port = true;
+      console.log("Sent command to port " + index + ".");
+      // Everyone gets the pause messages but only one should play
+      if (message.play) {
+        break;
+      }
+    } catch (err) {
+      // console.log([err]);
+      if (err.message.startsWith("Cannot read property 'postMessage' of undefined")) {
+
+      } else if (err.message.startsWith("Attempting to use a disconnected port object")) {
+
+      } else {
+        console.log("Encountered error while requesting play.");
+        console.log(err);
+      }
+      persistent_ports[index] = undefined;
+      
+    }
+  }
+  if (!found_port) {
+    console.log("Did not find port.")
     port_queue.push(message);
   }
+  return found_port;
+}
+
+var badge_state = "has no song";
+function setBadgeFromStatus(statuses) {
+  var is_playing = false;
+  var has_song = false;
+  for (var i = 0; i < statuses.length; i++) {
+    if (statuses[i] === undefined) {
+      continue;
+    }
+    if (statuses[i].play_state == "playing") {
+      is_playing = true;
+    }
+    if (statuses[i].play_state == "paused" || statuses[i].play_state == "playing") {
+      has_song = true;
+    }
+  }
+  if (!has_song) {
+    badge_state = "has no song";
+    var icon_dict = {'19':chrome.extension.getURL("images/no_song_19.png"),
+                     '38':chrome.extension.getURL("images/no_song_38.png")
+                    };
+  } else {
+    if (is_playing) {
+      badge_state = "playing";
+      var icon_dict = {'19':chrome.extension.getURL("images/pause_19.png"),
+                       '38':chrome.extension.getURL("images/pause_38.png")
+                      };
+    } else {
+      badge_state = "paused";
+      var icon_dict = {'19':chrome.extension.getURL("images/play_19.png"),
+                       '38':chrome.extension.getURL("images/play_38.png")
+                      };
+    }
+    // var icon_dict = chrome.extension.getURL(is_playing : "images/pause.png", "images/play.png");
+  }
+  chrome.browserAction.setIcon({path:icon_dict});
 }
 
 chrome.runtime.onConnect.addListener(function(port) {
@@ -33,54 +127,77 @@ chrome.runtime.onConnect.addListener(function(port) {
   // port.onMessage.addListener(function(msg) {
   //   console.log("Got message " + msg);
   // });
-  persistent_port = port;
   waiting_for_port_to_open = false;
   for (var i = 0; i < port_queue.length; i++) {
-    persistent_port.postMessage(port_queue[i]);
+    port.postMessage(port_queue[i]);
     port_queue = [];
   }
+  var port_id = persistent_ports.length;
+  port.onMessage.addListener(function(msg) {
+
+    console.assert(msg.status !== undefined);
+    if (persistent_ports_status[port_id] !== msg.status) {
+      // console.log(persistent_ports_status)
+      persistent_ports_status[port_id] = undefined;
+      persistent_ports_status[port_id] = msg.status;// {"play_state":msg.status.play_state, "has_focus":!!msg.status.has_focus};
+      // console.log(msg.status)
+      // console.log(persistent_ports_status)
+      var d = new Date();
+      persistent_ports_last_change[port_id] = d.getTime();
+      console.log(JSON.stringify(persistent_ports_status));
+      setBadgeFromStatus(persistent_ports_status);
+    }
+
+    
+
+  });
+  port.onDisconnect.addListener(function(){
+    persistent_ports_status[port_id] = {play_status :"disconnected", "has_focus":false};
+    var d = new Date();
+    persistent_ports_last_change[port_id] = d.getTime();
+    console.log(persistent_ports_status);
+    setBadgeFromStatus(persistent_ports_status);
+  });
+  persistent_ports.push(port);
+  persistent_ports_status.push(undefined);
+  persistent_ports_last_change.push(-1);
+  console.assert(persistent_ports.length == persistent_ports_status.length && persistent_ports.length == persistent_ports_last_change.length);
 });
 
+var USE_NEW_WINDOW = false;
 chrome.browserAction.onClicked.addListener(function(tab) {
   chrome.tabs.query({active:true, currentWindow: true}, function(tabs) {
     var original_tab_id = tabs[0].id;
 
-    if (persistent_port !== undefined || waiting_for_port_to_open) {
-      // Don't try to open a new tab
-      requestPlay();
+    // if (persistent_port !== undefined || waiting_for_port_to_open) {
+    
+    var found_port = requestPlay();
+    // Attempt to send a play request.
+    if (found_port || waiting_for_port_to_open) {
       return;
-    } else {
-      waiting_for_port_to_open = true;
     }
-    // Try to open a new tab somewhere
-    var use_new_window = true;
-    if (use_new_window) {
+    waiting_for_port_to_open = true;
+    // Try to open a music tab
+    
+    if (USE_NEW_WINDOW) {
       chrome.windows.getCurrent(function(original_window) { 
-        chrome.windows.create({url: getMusicUrl(), 
-                               focused : false, 
-                               left    : original_window.left, 
-                               top     : original_window.top/*, 
-                               width   : original_window.width, 
-                               height  : original_window.height*/}, 
-                              function(window) {
-          var music_tab_id = window.tabs[0].id;
-          chrome.windows.update(original_window.id, { focused: true}, function() {
-            // try_to_play(music_tab_id, function() {
-            //   setTimeout(function(){chrome.tabs.move(music_tab_id, {windowId: original_window.id, index:0});}, 10000);
-            // });
+        chrome.windows.create(
+          {
+            url     : getMusicUrl(), 
+            focused : false, 
+            left    : original_window.left, 
+            top     : original_window.top/*, 
+            width   : original_window.width, 
+            height  : original_window.height*/
+          }, 
+          function(window) {
+            // var music_tab_id = window.tabs[0].id;
+            chrome.windows.update(original_window.id, { focused: true}, function() {
           });
-          requestPlay();
-          // try_to_play(music_tab_id, function() {
-          //   // chrome.windows.update(original_window.id, { focused: true}, function() {
-          //   //   setTimeout(function(){chrome.tabs.move(music_tab_id, {windowId: original_window.id, index:0})}, 15000);
-          //   // });
-          //   // setTimeout(function(){chrome.tabs.update(original_tab_id, {active: true});}, 5000);
-          // });
         });
       });
     } else {
       chrome.tabs.create({url: getMusicUrl(), pinned:true, active:true}, function(tab) {
-        requestPlay();
       });
     }
   });
--- extension/content_script.js
+++ extension/content_script.js
@@ -1,17 +1,53 @@
-var SHUFFLE_SELECTOR = "[data-id='shuffle-my-library']"
+var LOAD_CHECKING_SELECTOR = "[data-id='shuffle-my-library']"
+var INITIAL_SELECTOR = "#iflFab";
+var DEFAULT_SELECTOR = "[data-id='shuffle-my-library']"
+
+var visible = true;
+
+$(window).focus(function() {
+  visible = true;
+  console.log("set visible to true");
+  sendStatusIfChanged(true);
+});
+
+$(window).blur(function() {
+  visible = false;
+  console.log("set visible to false");
+  sendStatusIfChanged(true);
+});
 
 function try_to_play() {
 	if ($("#player-bar-play-pause").css("pointer-events") === "none") {
 	  // Wait a bit so that the play button for the most recent song has time to load
-	  setTimeout(function(){$(SHUFFLE_SELECTOR).click();}, 1000);
+	  if ($(INITIAL_SELECTOR).first().length) {
+	  	setTimeout(function(){
+	  		$(INITIAL_SELECTOR).click();
+	  	  setTimeout(sendStatusIfChanged, 100)
+      }, 1000);
+	  } else {
+	  	// Shuffle seems to be available everywhere so just use that as the backup.
+	  	setTimeout(function(){
+        $(DEFAULT_SELECTOR).click();
+	  	  setTimeout(sendStatusIfChanged, 100);
+      }, 1000);
+	  }
 	} else {
 	  $("#player-bar-play-pause").click();
+	  setTimeout(sendStatusIfChanged, 100)
 	}
 	// $("#haha-custom-loading-div").remove();
 }
 
+function try_to_pause() {
+  var is_playing = $("#player-bar-play-pause[title^='Pause']").length > 0;
+	if (is_playing) {
+    $("#player-bar-play-pause").click();
+	  setTimeout(sendStatusIfChanged, 100)
+  }
+}
+
 function wait_for_load(callback) {
-	if ($(SHUFFLE_SELECTOR).get().length === 0) {
+	if ($(LOAD_CHECKING_SELECTOR).get().length === 0) {
 		setTimeout(function(){wait_for_load(callback)}, 100);
 		console.log("Waiting for load...");
 		return;
@@ -22,6 +58,43 @@ function wait_for_load(callback) {
 
 var port = chrome.runtime.connect({name: "audio"});
 port.onMessage.addListener(function(msg) {
-  console.log("Got click");
-  wait_for_load(try_to_play);
+  console.log(msg);
+  if (msg.play === true) {
+  	wait_for_load(try_to_play);
+  } else {
+  	wait_for_load(try_to_pause);
+  }
 });
+
+var status = "";
+var last_visible = undefined;
+function sendStatusIfChanged(force_message = false) {
+  var has_no_song = $("#currently-playing-title").length == 0;
+  // if (!has_no_song && status != "has no song") {
+  //   console.log("Currently playing:", $("#currently-playing-title").text())
+  // }
+	var is_playing = $("#player-bar-play-pause[title^='Pause']").length > 0;
+	var new_status = "";
+	if (has_no_song) {
+		new_status = "has no song"
+	} else {
+		new_status = is_playing ? "playing" : "paused"
+	}
+	if ((new_status != status) || (last_visible != visible) || force_message) {
+		status = new_status;
+    last_visible = visible;
+		port.postMessage({"status": {"play_state" : status, "has_focus":!!last_visible}});
+	}
+}
+
+
+function repeatedlySendStatus() {
+	sendStatusIfChanged();
+	setTimeout(repeatedlySendStatus, 1000);
+}
+
+repeatedlySendStatus();
+
+
+
+
